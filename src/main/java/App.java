5b03b528128e30b1cdf89bed20217c00a1627bdb
partko <partko.vl.3@gmail.com>
2024-05-20 17:18:10 +0300

    upload

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class App extends HttpServlet {
+    public String getGreeting() {
+        return "Hello ";
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // https://www.oracle.com/technetwork/java/servlet-142430.html
+        PrintWriter out = resp.getWriter();
+        out.println(this.getGreeting());
+        out.close();
+    }
+}
