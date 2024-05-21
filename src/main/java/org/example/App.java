f1525fe60117a147eaeb544521df6e28a8628ecb
partko <partko.vl.3@gmail.com>
2024-05-22 01:17:13 +0300

    l

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import java.io.IOException;
 
-@WebServlet(name = "app", value = "/app")
+@WebServlet(name = "app", value = "/aston")
 public class App extends HttpServlet {
     public String message = "Hello ";
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
